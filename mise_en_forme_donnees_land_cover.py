b553fec91c119fbfa76c79fec5284115d4e2af79
edouard61430-agro <edouard61430@gmail.com>
2026-06-09 16:41:26 +0100

    Question3

--- mise_en_forme_donnees_land_cover.py
+++ mise_en_forme_donnees_land_cover.py
@@ -1,5 +1,5 @@
 import geopandas as gpd
-
+import numpy as np
 
 try:
     land_use = gpd.read_file('CLC_RARA_RGF_SHP/CLC90/CLC90_RARA_RGF.shp')
@@ -7,3 +7,14 @@ except FileNotFoundError:
     raise FileNotFoundError("Le fichier n'existe pas au lien donné.")
 
 print("ça marche !")
+
+
+classes = np.unique(land_use['CODE_90'])
+nb_class_dist = len(classes)
+print(nb_class_dist)
+
+occurences = np.array([
+    (land_use['CODE_90'] == c).sum()
+    for c in classes
+])
+print(occurences)
