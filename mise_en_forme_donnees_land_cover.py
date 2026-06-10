4594f69280186d1e7fed2f127c4f20252c6c11d8
Antonin Rivron <antonin.rivron@gmail.com>
2026-06-10 14:09:16 +0200

    Question 4

--- mise_en_forme_donnees_land_cover.py
+++ mise_en_forme_donnees_land_cover.py
@@ -18,3 +18,41 @@ occurences = np.array([
     for c in classes
 ])
 print(occurences)
+
+def get_agg_code(fichier: gpd.GeoDataFrame, colonne: str) -> np.ndarray:
+    """
+    Agrège les codes d'une colonne en conservant uniquement leur premier chiffre.
+
+    Paramètres :
+        fichier : geopandas.GeoDataFrame
+        GeoDataFrame contenant les données.
+    colonne : str
+        Nom de la colonne contenant les codes à agréger.
+
+    Retourne : 
+        np.ndarray
+        Tableau NumPy contenant les codes agrégés sur un chiffre.
+    Lève :
+        TypeError
+            Si fichier n'est pas un GeoDataFrame.
+        KeyError
+            Si la colonne n'est pas présente dans le GeoDataFrame.
+    """
+
+    if not isinstance(fichier, gpd.GeoDataFrame):
+        raise TypeError("Le paramètre 'fichier' doit être un GeoDataFrame.")
+
+    if colonne not in fichier.columns:
+        raise KeyError(f"La colonne '{colonne}' est absente.")
+
+    # Convertit la colonne en format str et 
+    # agrège alors les chaînes de caractères en conservant uniquement le premier caractère
+    fichier[colonne] = fichier[colonne].astype(str).str[0]
+    
+    #Convertit à nouveau en chiffre afin d'avoir le code en format numérique
+    fichier[colonne] = fichier[colonne].astype(int)
+
+    return fichier
+
+land_use = get_agg_code(land_use, 'CODE_90')
+print(land_use.head())
