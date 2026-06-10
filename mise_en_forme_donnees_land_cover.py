0d3afcccb84e25589b97eab1ce93db7b31bdb310
edouard61430-agro <edouard61430@gmail.com>
2026-06-10 13:55:47 +0100

    Question 7

--- mise_en_forme_donnees_land_cover.py
+++ mise_en_forme_donnees_land_cover.py
@@ -1,5 +1,7 @@
 import geopandas as gpd
 import numpy as np
+import pytest
+
 
 try:
     land_use = gpd.read_file('CLC_RARA_RGF_SHP/CLC90/CLC90_RARA_RGF.shp')
@@ -8,6 +10,8 @@ except FileNotFoundError:
 
 print("ça marche !")
 
+print(land_use.columns)
+print(land_use.head())
 
 classes = np.unique(land_use['CODE_90'])
 nb_class_dist = len(classes)
@@ -19,9 +23,11 @@ occurences = np.array([
 ])
 print(occurences)
 
+
 def get_agg_code(fichier: gpd.GeoDataFrame, colonne: str) -> np.ndarray:
     """
-    Agrège les codes d'une colonne en conservant uniquement leur premier chiffre.
+    Agrège les codes d'une colonne en conservant uniquement
+    leur premier chiffre.
 
     Paramètres :
         fichier : geopandas.GeoDataFrame
@@ -29,7 +35,7 @@ def get_agg_code(fichier: gpd.GeoDataFrame, colonne: str) -> np.ndarray:
     colonne : str
         Nom de la colonne contenant les codes à agréger.
 
-    Retourne : 
+    Retourne :
         np.ndarray
         Tableau NumPy contenant les codes agrégés sur un chiffre.
     Lève :
@@ -45,14 +51,46 @@ def get_agg_code(fichier: gpd.GeoDataFrame, colonne: str) -> np.ndarray:
     if colonne not in fichier.columns:
         raise KeyError(f"La colonne '{colonne}' est absente.")
 
-    # Convertit la colonne en format str et 
-    # agrège alors les chaînes de caractères en conservant uniquement le premier caractère
+    # Convertit la colonne en format str et
+    # agrège alors les chaînes de caractères en conservant
+    # uniquement le premier caractère
     fichier[colonne] = fichier[colonne].astype(str).str[0]
-    
-    #Convertit à nouveau en chiffre afin d'avoir le code en format numérique
+
+    # Convertit à nouveau en chiffre afin d'avoir le code en format numérique
     fichier[colonne] = fichier[colonne].astype(int)
 
+    # Transcrit les codes en libelle
+    fichier[colonne] = fichier[colonne].apply(correspondance_code_libelle)
+
     return fichier
+
+
+def correspondance_code_libelle(code):
+    dico = {
+        1: "Territoires artificialisés",
+        2: "Territoires agricoles",
+        3: "Forêts et milieux semi-naturels",
+        4: "Zones humides",
+        5: "Surfaces en eau"
+        }
+
+    if code not in dico:
+        raise ValueError(f"Le code {code} est inconnu")
+
+    return dico[code]
+
+
+def test_code():
+    assert correspondance_code_libelle(1) == "Territoires artificialisés"
+
+
+def test_code_invalide():
+    with pytest.raises(ValueError):
+        correspondance_code_libelle(6)
+
+
+test_code()
+test_code_invalide()
 
 land_use = get_agg_code(land_use, 'CODE_90')
 print(land_use.head())
